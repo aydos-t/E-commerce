1d32eb22c20c5b0642ac2b262ef0ad7a13311b0f
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2024-04-29 13:18:35 +0500

    Create Login page

--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -1,12 +1,45 @@
 <template>
-  <h1>Login form</h1>
-</template>
+  <title>TorgoVit - login</title>
 
-<script>
-export default {
-  name: "Login"
-}
-</script>
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="200" height="200" viewBox="0 0 3222.222222222222 1681.965053718361">
+
+        <g transform="scale(11.111111111111109) translate(10, 10)">
+          <defs id="SvgjsDefs1249"></defs><g id="SvgjsG1250" featureKey="symbolFeature-0" transform="matrix(1.4371257922730525,0,0,1.4371257922730525,88.43113766915442,-10.778443442047895)" fill="#be3144"><path xmlns="http://www.w3.org/2000/svg" d="M61.7,51.2V19.7c0-1.5-1.2-2.8-2.8-2.8H47.2l1-3.4h0L49.6,9h4.1V7.5h-5.2l-1.8,6H17.1l1,3.4H6c-1.5,0-2.8,1.2-2.8,2.8v31.5  h-3.5v2.5l2.8,3.9h60l2.8-3.9v-2.5H61.7z M28.1,27.2l-0.8-5.3h4.6v5.3H28.1z M26.6,27.2h-3.8l-1.6-5.3h4.6L26.6,27.2z M31.9,20.3  h-4.9L26.3,15h5.7V20.3z M33.4,15h5.7l-0.8,5.3h-4.9V15z M33.4,21.8h4.6l-0.8,5.3h-3.8V21.8z M39.6,21.8h4.6l-1.6,5.3h-3.7  L39.6,21.8z M44.6,20.3h-4.8l0.8-5.3h5.6L44.6,20.3z M24.8,15l0.8,5.3h-4.8L19.1,15H24.8z M4.8,19.7c0-0.7,0.6-1.3,1.3-1.3h12.6  l3.1,10.3h20.4l-1.2,3.9c-0.1,0-0.2,0-0.3,0H25.5c-1.9,0-3.5,1.6-3.5,3.5c0,1.9,1.6,3.5,3.5,3.5c1.9,0,3.5-1.6,3.5-3.5  c0-0.8-0.2-1.4-0.6-2h9.4c-0.4,0.6-0.6,1.3-0.6,2c0,1.9,1.6,3.5,3.5,3.5c1.9,0,3.5-1.6,3.5-3.5c0-1.3-0.7-2.5-1.8-3.1l1.3-4.3h0  l3.1-10.3H59c0.7,0,1.3,0.6,1.3,1.3v31.5H4.8V19.7z M40.6,34c1.1,0,2,0.9,2,2c0,1.1-0.9,2-2,2c-1.1,0-2-0.9-2-2  C38.6,34.9,39.5,34,40.6,34z M27.5,36c0,1.1-0.9,2-2,2c-1.1,0-2-0.9-2-2c0-1.1,0.9-2,2-2C26.6,34,27.5,34.9,27.5,36z M63.8,53.2  l-2,2.8H3.3l-2-2.8v-0.5h62.5V53.2z"></path></g><g id="SvgjsG1251" featureKey="nameFeature-0" transform="matrix(1.406316052499376,0,0,1.406316052499376,-0.33751620130340027,74.78669452162283)" fill="#be3144"><path d="M12.52 17.28 l0 20.12 c0 1.52 -0.8 2.72 -2.52 2.72 s-2.52 -1.2 -2.52 -2.72 l0 -20.12 l-4.64 0 c-1.44 0 -2.6 -0.76 -2.6 -2.4 s1.16 -2.4 2.6 -2.4 l14.32 0 c1.44 0 2.6 0.76 2.6 2.4 s-1.16 2.4 -2.6 2.4 l-4.64 0 z M48.272999999999996 26.240000000000002 c0 8.96 -6.16 14 -13 14 s-13 -5.04 -13 -14 s6.16 -14 13 -14 s13 5.04 13 14 z M42.593 26.240000000000002 c0 -5.28 -2.96 -9.44 -7.32 -9.44 s-7.32 4.16 -7.32 9.44 s2.96 9.44 7.32 9.44 s7.32 -4.16 7.32 -9.44 z M58.106 25.16 l0 12.28 c0 1.48 -0.8 2.68 -2.52 2.68 s-2.52 -1.2 -2.52 -2.68 l0 -22.28 c0 -1.48 0.8 -2.68 2.52 -2.68 l8.56 0 c6.08 0 8.4 1.76 8.4 5.68 c0 2.72 -1.52 4.88 -3.64 6.28 c3.44 3.12 5.28 8.24 5.28 12.08 c0 1.84 -0.36 3.6 -2.36 3.6 c-2.2 0 -2.64 -1.48 -2.68 -3.36 c-0.12 -6.04 -3.4 -11.6 -11.04 -11.6 z M58.106 17.04 l0 3.8 c2.2 0 3.24 0.24 4.24 0.44 l2.44 0.64 c1.08 0 2.72 -1.8 2.72 -3 c0 -1.28 -1 -1.88 -2.72 -1.88 l-6.68 0 z M102.619 23.84 l0 13.28 c0 2 -1.12 3.12 -4.8 3.12 c-16.08 0 -20.32 -9.92 -20.56 -15.56 c-0.24 -7.04 4.04 -12.2 11.24 -12.2 l10.16 0 c1.44 0 2.6 0.76 2.6 2.4 s-1.16 2.4 -2.6 2.4 l-8.76 0 c-4.8 0 -6.96 2.56 -6.96 6.52 c0 7.64 6.56 11.64 14.64 11.64 l0 -10 l-6.08 0 c-1.36 0 -2.48 -0.72 -2.48 -2.28 s1.12 -2.28 2.48 -2.28 l7.68 0 c2.8 0 3.44 1.36 3.44 2.96 z M132.972 26.240000000000002 c0 8.96 -6.16 14 -13 14 s-13 -5.04 -13 -14 s6.16 -14 13 -14 s13 5.04 13 14 z M127.29200000000002 26.240000000000002 c0 -5.28 -2.96 -9.44 -7.32 -9.44 s-7.32 4.16 -7.32 9.44 s2.96 9.44 7.32 9.44 s7.32 -4.16 7.32 -9.44 z M147.445 34.68 c3.28 -5.76 4.88 -11.72 5.52 -18.32 c0.24 -2.44 0.4 -4 2.92 -4 c1.4 0 2.52 0.8 2.52 2.88 c0 5.48 -3.28 16.04 -6.12 20.84 c-1.36 2.28 -2.32 4.16 -4.84 4.16 s-3.48 -1.88 -4.84 -4.16 c-2.84 -4.8 -6.12 -15.36 -6.12 -20.84 c0 -2.08 1.12 -2.88 2.52 -2.88 c2.52 0 2.68 1.56 2.92 4 c0.64 6.6 2.24 12.56 5.52 18.32 z M168.598 15.079999999999998 l0 22.32 c0 1.52 -0.8 2.72 -2.52 2.72 s-2.52 -1.2 -2.52 -2.72 l0 -22.32 c0 -1.52 0.8 -2.72 2.52 -2.72 s2.52 1.2 2.52 2.72 z M184.991 17.28 l0 20.12 c0 1.52 -0.8 2.72 -2.52 2.72 s-2.52 -1.2 -2.52 -2.72 l0 -20.12 l-4.64 0 c-1.44 0 -2.6 -0.76 -2.6 -2.4 s1.16 -2.4 2.6 -2.4 l14.32 0 c1.44 0 2.6 0.76 2.6 2.4 s-1.16 2.4 -2.6 2.4 l-4.64 0 z"></path></g>
+        </g>
+      </svg>
+      <h2 class="text-center text-2xl font-bold tracking-tight text-gray-900">Войдите в аккаунт</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="" method="POST">
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900 text-left">Email</label>
+          <div class="mt-2">
+            <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Пароль</label>
+            <div class="text-sm">
+              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Забыли пароль?</a>
+            </div>
+          </div>
+          <div class="mt-2">
+            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Войти</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
 
 <style scoped>
 
