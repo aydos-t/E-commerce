936075606e607d9fe7f3cfc59076badd3c5a265e
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2024-05-06 20:50:24 +0500

    Implement redirect to login if token does not exist

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -5,6 +5,7 @@ import Dashboard from "../views/Dashboard.vue";
 import RequestPassword from "../views/RequestPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
 import Products from "../views/Products.vue";
+import store from "../store";
 
 
 const routes = [
@@ -12,6 +13,9 @@ const routes = [
         path: '/app',
         name: 'app',
         component: AppLayout,
+        meta: {
+            requiresAuth: true
+        },
         children: [
             {
                 path: 'dashboard',
@@ -28,23 +32,44 @@ const routes = [
     {
         path: '/login',
         name: 'login',
-        component: Login
+        component: Login,
+        meta: {
+            requiresGuest: true
+        }
     },
     {
         path: '/request-password',
         name: 'requestPassword',
-        component: RequestPassword
+        component: RequestPassword,
+        meta: {
+            requiresGuest: true
+        }
     },
     {
         path: '/reset-password/:token',
         name: 'resetPassword',
-        component: ResetPassword
+        component: ResetPassword,
+        meta: {
+            requiresGuest: true
+        }
     },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    console.log(to);
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({name: 'login'})
+    } else if (to.meta.requiresGuest && store.state.user.token) {
+        next({name: 'app.dashboard'})
+    } else {
+        next();
+    }
+
 })
 
 export default router;
