922f4f86171711ff7e1934aa212ce3d4050127f9
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2024-05-13 18:02:36 +0500

    Implement Product CRUD Create ProductListResource and ProductResource files

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('/user', [AuthController::class, 'getUser']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('products', ProductController::class);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
